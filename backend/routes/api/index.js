b12897ebc63306cd595202e1bf563b930b32b11e
stevenseb <stevenseb@hotmail.com>
2024-04-15 16:31:34 -0400

    completed authme phase 1

---
+++ backend/routes/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+// test route for api router
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+  });
+
+
+
+
+module.exports = router;
