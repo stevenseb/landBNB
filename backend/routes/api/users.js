01bd9f5b49b2317651e7888b5bf1db077e529d83
stevenseb <stevenseb@hotmail.com>
2024-04-17 18:15:44 -0400

    checked signup validations

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+const { check } = require('express-validator');
 const { validateSignup } = require('../../utils/validation');
 const { User } = require('../../db/models');
 
