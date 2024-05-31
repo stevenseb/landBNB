ae366a182928b8126f7b756f66b1fe68a79271f9
stevenseb <stevenseb@hotmail.com>
2024-05-30 20:15:21 -0400

    added buld files for production

--- backend/routes/OLDindex.js
+++ backend/routes/OLDindex.js
@@ -1,10 +1,14 @@
-/ backend/routes/index.js
+// backend/routes/index.js
 const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
-
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
+ 
 // Static routes
 // Serve React build files in production
 if (process.env.NODE_ENV === 'production') {
@@ -12,32 +16,39 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at the root route
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.sendFile(
+    return res.sendFile(
       path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
     );
   });
 
   // Serve the static assets in the frontend's build folder
-  router.use(express.static(path.resolve("../frontend/dist")));
+  router.use(express.static(path.resolve("../frontend/build")));
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.sendFile(
-      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
 }
 
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
-  router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
   });
 }
+
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
+});
+
+
 
 module.exports = router;
--- frontend/src/components/NavBar/ProfileButton.jsx
+++ frontend/src/components/NavBar/ProfileButton.jsx
@@ -1,32 +1,54 @@
 // frontend/src/components/Navigation/ProfileButton.jsx
 
 import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { FaUserCircle } from 'react-icons/fa';
 import * as sessionActions from '../../store/session';
+import './NavBar.css';
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
 
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
   };
 
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  const closeMenu = () => {
+    if (!showMenu) return;
+    setShowMenu(false);
+  };
+
   return (
-    <>
-      <button>
+    <div className="profile-button-container" onMouseLeave={closeMenu}>
+      <button onClick={openMenu} className="profile-button">
         <FaUserCircle />
       </button>
-      <ul className="profile-dropdown">
-        <li>{user.username}</li>
-        <li>{user.firstName} {user.lastName}</li>
-        <li>{user.email}</li>
-        <li>
-          <button onClick={logout}>Log Out</button>
-        </li>
-      </ul>
-    </>
+      {showMenu && (
+        <ul className="profile-dropdown">
+          <li className="profile-info">
+            <div>{user.username}</div>
+            <div>{user.firstName} {user.lastName}</div>
+            <div>{user.email}</div>
+          </li>
+          <li>
+            <NavLink to="/profile" className="profile-dropdown-item" onClick={closeMenu}>
+              Profile
+            </NavLink>
+          </li>
+          <li>
+            <button onClick={logout} className="profile-dropdown-item">Log Out</button>
+          </li>
+        </ul>
+      )}
+    </div>
   );
 }
 
