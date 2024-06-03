87524b25b150fe29dc91721f3777cf0875720383
stevenseb <stevenseb@hotmail.com>
2024-06-03 10:31:36 -0400

    changed spot image url datatype

--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
     },
     url: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull: false,
       validate: {
         isUrl: true,
